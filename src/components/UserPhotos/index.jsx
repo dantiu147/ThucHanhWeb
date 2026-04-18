fa0288a54277f5e869baf19d9bd50e55aa40c5fd
Nguyễn Tiến Đàn <tiudan147@gmail.com>
2026-04-18 08:39:46 +0000

    fix: cập nhật code và sửa lỗi môi trường

--- src/components/UserPhotos/index.jsx
+++ src/components/UserPhotos/index.jsx
@@ -1,23 +1,33 @@
 import React from "react";
-import { CardMedia, Typography, ListItem,  List, Card, CardHeader, Divider, CardContent} from "@mui/material";
+import {
+  CardMedia,
+  Typography,
+  ListItem,
+  List,
+  Card,
+  CardHeader,
+  Divider,
+  CardContent,
+} from "@mui/material";
 
 import "./styles.css";
-import {Link, useParams} from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import models from "../../modelData/models";
 
 /**
  * Define UserPhotos, a React component of Project 4.
  */
-function UserPhotos () {
-    const { userId } = useParams();
-    const photos = models.photoOfUserModel(userId)
-    return (
+function UserPhotos() {
+  const { userId } = useParams();
+  const photos = models.photoOfUserModel(userId);
+  // vừa thử
+  return (
     <div className="user-photos-container">
       {photos.map((photo) => (
         <Card key={photo._id} style={{ marginBottom: "20px" }}>
           {/* 1. Thông tin ảnh: Ngày giờ đăng */}
-          <CardHeader 
-            title={`Posted on: ${new Date(photo.date_time).toLocaleString()}`} 
+          <CardHeader
+            title={`Posted on: ${new Date(photo.date_time).toLocaleString()}`}
           />
 
           {/* 2. Hiển thị ảnh */}
@@ -36,7 +46,11 @@ function UserPhotos () {
             <List>
               {photo.comments && photo.comments.length > 0 ? (
                 photo.comments.map((comment) => (
-                  <ListItem key={comment._id} alignItems="flex-start" style={{ flexDirection: "column" }}>
+                  <ListItem
+                    key={comment._id}
+                    alignItems="flex-start"
+                    style={{ flexDirection: "column" }}
+                  >
                     <Typography variant="subtitle2">
                       {/* Link dẫn tới UserDetail của người comment */}
                       <Link to={`/users/${comment.user._id}`}>
@@ -46,15 +60,20 @@ function UserPhotos () {
                         {new Date(comment.date_time).toLocaleString()}
                       </span>
                     </Typography>
-                    
+
                     <Typography variant="body2" style={{ marginTop: "5px" }}>
                       {comment.comment}
                     </Typography>
-                    <Divider variant="fullWidth" style={{ width: "100%", marginTop: "10px" }} />
+                    <Divider
+                      variant="fullWidth"
+                      style={{ width: "100%", marginTop: "10px" }}
+                    />
                   </ListItem>
                 ))
               ) : (
-                <Typography variant="body2" color="textSecondary">No comments yet.</Typography>
+                <Typography variant="body2" color="textSecondary">
+                  No comments yet.
+                </Typography>
               )}
             </List>
           </CardContent>
